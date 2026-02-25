a987f9d64e602b1d9243392bcced74437382335a
fullstack-rishi <barverushikesh754@gmail.com>
2026-02-25 12:47:12 +0530

    first commit

--- src/footer/Footer.tsx
+++ src/footer/Footer.tsx
@@ -3,7 +3,7 @@ export default function Footer() {
   const d:Date = new Date();
   return (
     <div className="flex item-center justify-center text-white">
-      © 2026-{d.getFullYear()} Copyright Rushikesh barve
+      © 2025-{d.getFullYear()} Copyright Rushikesh Barve
     </div>
   )
 }
